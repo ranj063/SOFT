7718e22d5d886caf75a28ff678dadbe7e6df4c2b
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2018-05-15 23:43:43 -0700

    tune: Add README and a script to configure testbench parameters
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

---
+++ tune/scripts/run_testbench.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#input file
+input_file="48000Hz_stereo_16bit.raw"
+
+#output_file
+output_file="out.raw"
+
+#input bit format
+bits_in="S16_LE"
+
+# topology file
+# please use only simple volume topologies for now
+topology_file="../topology/test/test-playback-ssp2-I2S-volume-s16le-s32le-48k-24576k-codec.tplg"
+
+#optional libraries to override
+libraries="vol=libsof_volume.so"
+
+# Use -d to enable debug prints
+
+# run testbench
+./test/testbench -i $input_file -o $output_file -b $bits_in -t $topology_file -a $libraries -d
